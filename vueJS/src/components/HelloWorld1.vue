ec6ce1550968f6ce7ee664555260646d95a1ff68
tavandinh <tavandinh88@gmail.com>
2020-06-10 00:22:46 +0700

    exercise1-1

---
+++ vueJS/src/components/HelloWorld1.vue
@@ -0,0 +1,105 @@
+<style  scoped src="./css/helloWorld.css">
+</style>
+<template>
+  <div class="hello">
+    <h1>{{ array[0] }}</h1>
+    <h1>{{ objA.obj1 }}</h1>
+    <h1>{{ test }}</h1>
+    <h1>{{ message }}</h1>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links 1111</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['message', "test", "objA","array"],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
---
+++ vueJS/src/components/modules/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Hello1 from "../HelloWorld1"
+import Hello2 from "../HelloWorld2"
+import Hello3 from "../HelloWorld3"
+
+Vue.component('Hello1-component', Hello1);
+Vue.component('Hello2-component', Hello2);
+Vue.component('Hello3-component', Hello3);
